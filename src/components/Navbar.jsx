75d2f6ade2edd6c1edc6e246cc3effca84d906e8
unknown <otmax.vft@gmail.com>
2022-08-17 21:12:02 +0100

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,30 @@ const Navbar = () => {
 
     return (
         <>
-            <div className='p-4 flex justify-between items-center bg-neutral-50'>
-                <h1 className='mx-2 text-emerald-400 text-4xl font-bold cursor-pointer'>OTH<span className='text-slate-300'>DR</span></h1>
-                <ul className='hidden md:flex'>
-                    <li className='text-md mx-2 hover:font-semibold hover:text-emerald-400 text-slate-400'><a href="/">Pricing</a></li>
-                    <li className='text-md mx-2 hover:font-semibold hover:text-emerald-400 text-slate-400'><a href="/">Contact</a></li>
-                    <li className='text-md mx-2 hover:font-semibold hover:text-emerald-400 text-slate-400'><a href="/">About</a></li>
-                </ul>
+            <div className='flex flex-col'>
+                <div className='p-4 flex justify-between items-center bg-neutral-50'>
+                    <h1 className='mx-2 text-emerald-400 text-4xl font-bold cursor-pointer'>OTH<span className='text-slate-300'>DR</span></h1>
+                    <ul className='hidden md:flex'>
+                        <li className='text-md mx-2 hover:font-semibold hover:text-emerald-500 text-slate-400'><a href="/">Pricing</a></li>
+                        <li className='text-md mx-2 hover:font-semibold hover:text-emerald-500 text-slate-400'><a href="/">Contact</a></li>
+                        <li className='text-md mx-2 hover:font-semibold hover:text-emerald-500 text-slate-400'><a href="/">About</a></li>
+                    </ul>
 
-                {mobileNav
-                    ? <FiX onClick={toggleMenu} className='md:hidden cursor-pointer hover:text-emerald-200 text-slate-600' size={22} />
-                    : <FiMenu onClick={toggleMenu} className='md:hidden cursor-pointer hover:text-emerald-200 text-slate-600' size={22} />
-                }
+                    {mobileNav
+                        ? <FiX onClick={toggleMenu} className='md:hidden cursor-pointer hover:text-emerald-200 text-slate-600' size={22} />
+                        : <FiMenu onClick={toggleMenu} className='md:hidden cursor-pointer hover:text-emerald-200 text-slate-600' size={22} />
+                    }
 
+                </div>
+                {mobileNav && <div>
+                    <ul className='absolute md:hidden w-full h-screen bg-slate-900 pt-[20%] px-[30%] uppercase z-10'>
+                        <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">Pricing</a></li>
+                        <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">Contact</a></li>
+                        <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">About</a></li>
+                    </ul>
+
+                </div>}
             </div>
-            {mobileNav && <ul className='absolute md:hidden w-full h-screen bg-slate-900 pt-[20%] px-[30%] uppercase z-10'>
-                <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">Pricing</a></li>
-                <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">Contact</a></li>
-                <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">About</a></li>
-            </ul>}
         </>
     );
 };
