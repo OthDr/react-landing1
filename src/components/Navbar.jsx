0109de9760b4c0f58807c7740e2e3461ab1fcec0
unknown <otmax.vft@gmail.com>
2022-08-17 20:42:29 +0100

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
 const Navbar = () => {
 
-    const [ mobileNav, showNav ] = useState(false);
-    const toggleMenu = () => {showNav(!mobileNav); console.log('clickeeeed');};
+    const [mobileNav, showNav] = useState(false);
+    const toggleMenu = () => { showNav(!mobileNav); console.log('clickeeeed'); };
 
     return (
         <>
-            <div className='p-4 flex justify-between items-center bg-neutral-50  drop-shadow-md '>
+            <div className='p-4 flex justify-between items-center bg-neutral-50'>
                 <h1 className='mx-2 text-emerald-400 text-4xl font-bold cursor-pointer'>OTH<span className='text-slate-300'>DR</span></h1>
                 <ul className='hidden md:flex'>
                     <li className='text-md mx-2 hover:font-semibold hover:text-emerald-400 text-slate-400'><a href="/">Pricing</a></li>
@@ -15,16 +15,16 @@ const Navbar = () => {
                     <li className='text-md mx-2 hover:font-semibold hover:text-emerald-400 text-slate-400'><a href="/">About</a></li>
                 </ul>
 
-                {!mobileNav
+                {mobileNav
                     ? <FiX onClick={toggleMenu} className='md:hidden cursor-pointer hover:text-emerald-200 text-slate-600' size={22} />
                     : <FiMenu onClick={toggleMenu} className='md:hidden cursor-pointer hover:text-emerald-200 text-slate-600' size={22} />
                 }
 
             </div>
-            {!mobileNav && <ul className='absolute md:hidden w-full h-screen bg-neutral-50 pt-[20%] px-[30%] uppercase'>
-                <li className='text-md my-8 text-slate-600 hover:text-emerald-200 text-2xl text-center border-b-2 border-emerald-50 leading-10'><a className='shadow-2xl shadow-emerald-50' href="/">Pricing</a></li>
-                <li className='text-md my-8 text-slate-600 hover:text-emerald-200 text-2xl text-center border-b-2 border-emerald-50 leading-10'><a className='shadow-2xl shadow-emerald-50' href="/">Contact</a></li>
-                <li className='text-md my-8 text-slate-600 hover:text-emerald-200 text-2xl text-center border-b-2 border-emerald-50 leading-10'><a className='shadow-2xl shadow-emerald-50' href="/">About</a></li>
+            {mobileNav && <ul className='absolute md:hidden w-full h-screen bg-slate-900 pt-[20%] px-[30%] uppercase'>
+                <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">Pricing</a></li>
+                <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">Contact</a></li>
+                <li className='text-md my-8 text-slate-600 hover:text-emerald-400 text-2xl text-center border-b-2 border-slate-800 leading-10'><a className='' href="/">About</a></li>
             </ul>}
         </>
     );
